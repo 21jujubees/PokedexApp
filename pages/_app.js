62368f44031a889a579285155b2de5656b06d973
21jujubees <107655284+21jujubees@users.noreply.github.com>
2022-09-01 18:01:58 -0400

    the part where the thing is added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+      <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" 
+        crossorigin="anonymous"
+        ></link>
+    </Head>
+    <Component {...pageProps} />
+  </>);
 }
 
 export default MyApp
